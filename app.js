bc55b95a9797bb267573f584141476a674996aaa
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2022-08-18 13:52:14 -0700

    Added the rest of the features. Finished

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const gridContainer = document.querySelector('.container');
 const resetBtn = document.getElementById('reset-btn');
-const colorPrompt = document.querySelector('color')
+const rainbowBtn = document.getElementById('rainbow');
+const eraserBtn = document.getElementById('eraser');
+const blackBtn = document.getElementById('black-btn');
+const customHex = document.getElementById('custom-hex');
 let square;
-let pickedColor = "blue";
+let pickedColor = "black";
 
 function createGrid(size) { // works
     for (let i = 0; i < size; i++) {
@@ -21,13 +24,41 @@ function clearGrid() { // works
   gridContainer.textContent = '';
   let newsize = prompt("What size do you want the grid? : ");
   createGrid(newsize);
+  changeColor("#000000")
 }
 
-function changeColor() {
-  console.log("greg");
+function changeColor(color) {
+  //console.log("greg");
   let cells = Array.from(document.querySelectorAll('.grid-square'));
   cells.forEach(cell => cell.addEventListener('mouseover', () => {
-    cell.style.backgroundColor = pickedColor;
+    cell.style.backgroundColor = color;
+  }))
+}
+
+function randomColor() {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let pickedColor = randomColor;
+    console.log(typeof randomColor);
+    changeColor("#" + randomColor);
+
+}
+
+function customColor() { // works
+  let color = prompt("Enter hex code: ");
+  changeColor(color)
+}
+
+function eraser() {
+  let cells = Array.from(document.querySelectorAll('.grid-square'));
+  cells.forEach(cell => cell.addEventListener('mouseover', () => {
+    cell.style.backgroundColor = "white";
+  }))
+}
+
+function blackDefault() {
+  let cells = Array.from(document.querySelectorAll('.grid-square'));
+  cells.forEach(cell => cell.addEventListener('mouseover', () => {
+    cell.style.backgroundColor = "#000000";
   }))
 }
 
@@ -41,5 +72,10 @@ function changeColor() {
 
 document.addEventListener('DOMContentLoaded', () => {
     createGrid(16);
+    changeColor("#000000")
     resetBtn.addEventListener('click', clearGrid);
+    blackBtn.addEventListener('click', blackDefault);
+    customHex.addEventListener('click', customColor);
+    rainbowBtn.addEventListener('click', randomColor);
+    eraserBtn.addEventListener('click', eraser);
 });
